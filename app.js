0d15388add5b8b7ed3d421f771a069de5a6ec1f4
KyleHEng <kylehussaineng@gmail.com>
2025-01-28 08:45:18 +0000

    "Added general 404 and 500 error handling"

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.get("/api/topics", getTopics);
 
 //error handling middleware
 app.all("*", (req, res) => {
-
-  res.status(404).send({error: "Endpoint not found"})
-})
-
-
+  res.status(404).send({ error: "Endpoint not found" });
+});
 
 app.use((err, req, res, next) => {
-    
-})
+  //500 error expected when there are bugs in code, or there are server issues
+  //Might try to make test for this after finishing core tasks
+  console.log(err);
+  res.status(500).send({ msg: "Internal Server Error" });
+});
 module.exports = app;
