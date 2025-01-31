154cae889e3fb020495995802072d3fce9f86a66
KyleHEng <kylehussaineng@gmail.com>
2025-01-31 11:04:46 +0000

    "testing supabase"

--- listen.js
+++ listen.js
@@ -1,10 +1,4 @@
-const app = require("./app");
+const app = require("./app.js");
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`Listening on ${PORT}!`);
-  }
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
